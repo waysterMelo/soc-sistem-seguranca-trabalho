d0f47e44138321f05f367ce348bd7a8d0ffb4145
waysterMelo <waystermelo@gmail.com>
2025-04-12 22:04:26 -0300

    Initial commit

---
+++ src/test/java/com/ocupacional/soc/SocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocupacional.soc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
